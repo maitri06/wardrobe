8b6283557286e2aaa40e7bca9b59c625010dbb97
Darshil Patel <darshil_21@yahoo.in>
2018-01-16 17:13:38 +0530

    for email

--- model/email_model.js
+++ model/email_model.js
@@ -1,23 +1,23 @@
-var email   = require("emailjs/email");
-var demo={
+var email = require("emailjs/email");
+var demo = {
 
-sendMail:function(demo,callback){
+    sendMail: function(demo, callback) {
 
-var server  = email.server.connect({
-   user:    "darshil212221@gmail.com", 
-   password:"123456789*0#", 
-   host:    "smtp.gmail.com", 
-   ssl:     true,
-   port:465
-});
+        var server = email.server.connect({
+            user: "wishmytrip3@gmail.com",
+            password: "wish1234",
+            host: "smtp.gmail.com",
+            ssl: true,
+            port: 465
+        });
 
-server.send({
-   text:    demo.message,
-   from:    "darshil212221@gmail.com", 
-   to:     demo.name,
-   subject: demo.subject
-}, callback);
-}
+        server.send({
+            text: demo.message,
+            from: "wishmytrip3@gmail.com",
+            to: demo.name,
+            subject: demo.subject
+        }, callback);
+    }
 
 
 }
--- model/feedback_model.js
+++ model/feedback_model.js
@@ -7,13 +7,13 @@ var feedback={
     },
     getFeedbackById:function(id,callback){
 
-        return db.query("select * from feedback_tbl where f_id=?",[id],callback);
+        return db.query("select * from feedback_tbl where pk_f_id=?",[id],callback);
     },
     addFeedback:function(feed,callback){
-        return db.query("insert into feedback_tbl values(?,?,?,?)",[feed.f_id,feed.desc,feed.email_id,feed.pro_id],callback);
+        return db.query("insert into feedback_tbl values(?,?,?,?,?)",[feed.pk_f_id,feed.f_title,feed.f_desc,feed.fk_email_id,feed.fk_pro_id],callback);
     },
     deleteFeedback:function(id,callback){
-        return db.query("delete from feedback_tbl where f_id=?",[id],callback);
+        return db.query("delete from feedback_tbl where pk_f_id=?",[id],callback);
     },
    /*updateCat:function(id,cat,callback){
         return db.query("update cat_tbl set name=? where cat_id=?",[cat.name,id],callback);
