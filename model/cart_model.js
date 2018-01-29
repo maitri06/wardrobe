8cdfc0c3755147818291651793a9081a6bd53191
Darshil Patel <darshil_21@yahoo.in>
2018-01-29 18:37:13 +0530

    done

--- model/cart_model.js
+++ model/cart_model.js
@@ -7,13 +7,13 @@ var cart={
     },
     getCartById:function(id,callback){
 
-        return db.query("select * from cart_tbl where cart_id=?",[id],callback);
+        return db.query("select * from cart_tbl where pk_cart_id=?",[id],callback);
     },
     addCart:function(car,callback){
-        return db.query("insert into cart_tbl values(?,?,?,?)",[car.cart_id,car.qty,car.email_id,car.pro_id],callback);
+        return db.query("insert into cart_tbl values(?,?,?,?,?)",[null,car.cart_qty,car.cart_amount,car.fk_email_id,car.fk_pro_id],callback);
     },
     deleteCart:function(id,callback){
-        return db.query("delete from cart_tbl where cart_id=?",[id],callback);
+        return db.query("delete from cart_tbl where pk_cart_id=?",[id],callback);
     },
    /*updateCat:function(id,cat,callback){
         return db.query("update cat_tbl set name=? where cat_id=?",[cat.name,id],callback);
